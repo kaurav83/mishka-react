22514a28fcd30bfe2026fbb3fc2618e4e7dce979
Vitaliy <puga.vitaliy@gmail.com>
2018-07-13 14:27:28 +0300

    header мобайл и десктоп версии на bootstrap

---
+++ js/compile/common.js
@@ -0,0 +1,10 @@
+// class SearchElem{
+//     method() {
+//         document.querySelector('div').style.color = "green";
+//         document.querySelector('body').style.backgroundColor = "red";
+//     }
+// }
+
+// var searchElem = new SearchElem();
+// searchElem.method();
+"use strict";
