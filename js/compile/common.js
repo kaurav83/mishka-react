96f95439cca5fa390bd5efebbcb02cd9b7a4141c
Vitaliy <puga.vitaliy@gmail.com>
2018-07-25 17:11:55 +0300

    сверстал раздел Отзывы, сделал слайдер, анимированные кнопки

--- js/compile/common.js
+++ js/compile/common.js
@@ -1,10 +1,93 @@
-// class SearchElem{
-//     method() {
-//         document.querySelector('div').style.color = "green";
-//         document.querySelector('body').style.backgroundColor = "red";
-//     }
-// }
+'use strict';
 
-// var searchElem = new SearchElem();
-// searchElem.method();
-"use strict";
+// слайдер
+function sliderEngine(sliderDescription, prevItem, nextItem) {
+    var description = document.querySelectorAll(sliderDescription);
+    var prevButton = document.querySelector(prevItem);
+    var nextButton = document.querySelector(nextItem);
+    var currentItem = 0;
+
+    function next(e) {
+        if (currentItem === description.length - 1) {
+            description[currentItem].classList.remove('sliding');
+
+            currentItem = 0;
+            description[currentItem].classList.add('sliding');
+        } else {
+            showItem(this.id);
+        }
+    }
+
+    function prev(e) {
+        if (currentItem < description.length) {
+            if (currentItem <= 0) {
+                description[currentItem].classList.remove('sliding');
+                currentItem = description.length - 1;
+                description[currentItem].classList.add('sliding');
+            } else {
+                showItem(this.id);
+            }
+        }
+    }
+
+    function showItem(elemID) {
+        if (elemID === 'prev') {
+            description[currentItem].classList.remove('sliding');
+            currentItem--;
+            description[currentItem].classList.add('sliding');
+        } else if (elemID === 'next') {
+            description[currentItem].classList.remove('sliding');
+            currentItem++;
+            description[currentItem].classList.add('sliding');
+        }
+    }
+
+    if (nextButton) {
+        nextButton.addEventListener('click', next);
+    }
+
+    if (prevButton) {
+        prevButton.addEventListener('click', prev);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    sliderEngine('.reviews__item', '.reviews__slider-button--prev', '.reviews__slider-button--next');
+});
+
+//пульсирующая кнопка
+function pulse() {
+    var buttons = void 0,
+        buttonPulse = void 0;
+
+    buttons = document.querySelectorAll('.button');
+
+    buttonPulse = function buttonPulse(e) {
+        [].forEach.call(buttons, function (item) {
+            item.classList.remove('button--active');
+        });
+
+        this.classList.add('button--active');
+
+        var addElem = document.querySelector('.button__pulse');
+        var styleElem = addElem.style;
+
+        var maxValue = Math.max(this.clientWidth, this.clientHeight);
+        var px = 'px';
+        var rect = this.getBoundingClientRect();
+
+        styleElem.width = styleElem.height = maxValue + px;
+        styleElem.left = e.clientX - rect.left - maxValue / 2 + px;
+        styleElem.top = e.clientY - rect.top - maxValue / 2 + px;
+
+        this.appendChild(addElem, addElem);
+    };
+
+    Array.prototype.forEach.call(buttons, function (item) {
+        item.addEventListener('click', buttonPulse);
+    });
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    pulse();
+});
